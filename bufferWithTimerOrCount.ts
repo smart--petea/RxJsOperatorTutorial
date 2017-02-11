8e0c8c4ca9b4f86c60f82e592dd53eb8fede5041
petea <smart__petea@mail.ru>
2017-02-11 21:51:56 +0200

    adding buffer functionality

--- bufferWithTimerOrCount.ts
+++ bufferWithTimerOrCount.ts
@@ -2,29 +2,45 @@ import { Observable } from "rxjs/Observable";
 import { Observer } from "rxjs/Observer";
 import { Subscriber } from "rxjs/Subscriber";
 
-function bufferWithTimerOrCount<T>(): Observable<T> {
+function bufferWithTimerOrCount<T>(bufferSize: number): Observable<T> {
     //this is the Observable change by our operator
     let source: Observable<T> = this;
-    return Observable.create((subscriber: Subscriber<T>) => {
-        source.subscribe( new BufferWithTimerOrCountOperator(subscriber) );
+    return Observable.create((subscriber: Subscriber<Array<T>>) => {
+        source.subscribe( new BufferWithTimerOrCountOperator(subscriber, bufferSize) );
     });
 }
 
 class BufferWithTimerOrCountOperator<T> implements Observer<T> {
     closed: boolean = false; //the operator is open. If it is true, the oservable will not emit events to this subscriber
+    private list: Array<T>;
 
-    constructor(private subscriber: Subscriber<T>) { }
+    constructor(private subscriber: Subscriber<Array<T>>, private bufferSize: number) {
+        this.list = new Array<T>();
+    }
 
     public next(value: T): void {
-        this.subscriber.next(value);
+        this.list.push(value);
+        if(this.list.length >= this.bufferSize) {
+            let list = this.list;
+            this.list = new Array<T>(); //start new buffer
+            this.subscriber.next(list); //send the full buffer
+        }
     }
 
 
     public error(err: any): void {
+        if(this.list.length) {
+            this.subscriber.next(this.list); //send firstly the existent values
+        }
+
         this.subscriber.error(err);
     }
 
     public complete(): void {
+        if(this.list.length) {
+            this.subscriber.next(this.list); //send firstly the existent values
+        }
+
         this.subscriber.complete();
     }
 }
--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ let x = Rx.Observable
     .interval(200)   //every 200 ms
     //.bufferCount(3) //group the events in the 3-dimensional arrays
     //.bufferTime(500)  //group the events every 500 ms
-    .bufferWithTimerOrCount() //Let's implement it!!!
+    .bufferWithTimerOrCount(3) //Let's implement it!!!
     .subscribe(
         console.log, //onNext
         console.log  //onError
