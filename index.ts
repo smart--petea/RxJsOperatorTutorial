7d66e38c038441b9bca75e1aa892fa3d32186bf6
petea <smart__petea@mail.ru>
2017-02-11 20:53:32 +0200

    Interval and buffering by count

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import * as Rx from 'rxjs';
 
 Rx.Observable
     .interval(200)   //every 200 ms
+    .bufferCount(3) //group the events in the 3-dimensional arrays
     .subscribe(
         console.log, //onNext
         console.log  //onError
