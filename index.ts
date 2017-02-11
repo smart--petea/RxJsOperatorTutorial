73d5c8d34755fcca32cf664db442c97f6e4fb71b
petea <smart__petea@mail.ru>
2017-02-11 20:58:48 +0200

    bufferWithTimeOrCount operator does not exist

--- index.ts
+++ index.ts
@@ -4,7 +4,8 @@ import * as Rx from 'rxjs';
 Rx.Observable
     .interval(200)   //every 200 ms
     //.bufferCount(3) //group the events in the 3-dimensional arrays
-    .bufferTime(500)  //group the events every 500 ms
+    //.bufferTime(500)  //group the events every 500 ms
+    .bufferWithTimeOrCount(500, 3) //Let's implement it!!!
     .subscribe(
         console.log, //onNext
         console.log  //onError
