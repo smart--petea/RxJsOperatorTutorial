ea0e1e74615ec721d5191e79968eea2ffd5a9ee4
petea <smart__petea@mail.ru>
2017-02-11 21:33:50 +0200

    subscribe via implementation of Observer<T> interface

--- bufferWithTimerOrCount.ts
+++ bufferWithTimerOrCount.ts
@@ -1,15 +1,31 @@
 import { Observable } from "rxjs/Observable";
+import { Observer } from "rxjs/Observer";
 
 function bufferWithTimerOrCount<T>(): Observable<T> {
     //this is the Observable change by our operator
     let source: Observable<T> = this;
     return Observable.create(subscriber => {
-        source.subscribe(
-            x => subscriber.next(x),
-            err => subscriber.error(err),
-            () => subscriber.completed()
-        );
+        source.subscribe( new BufferWithTimerOrCountOperator(subscriber) );
     });
+}
+
+class BufferWithTimerOrCountOperator<T> implements Observer<T> {
+    closed: boolean = false; //the operator is open. If it is true, the oservable will not emit events to this subscriber
+
+    constructor(private subscriber) { }
+
+    public next(value: T): void {
+        this.subscriber.next(value);
+    }
+
+
+    public error(err: any): void {
+        this.subscriber.error(err);
+    }
+
+    public complete(): void {
+        this.subscriber.complete();
+    }
 }
 
 //Register our function in javascript.
